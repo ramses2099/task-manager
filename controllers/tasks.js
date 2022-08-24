f240db44fb236cab455346b72c2fa59f4cb001b3
ramses2099 <ramses2099@gmail.com>
2022-08-24 15:22:01 -0400

    new change

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,6 +2,27 @@ const getAllTasks = (req, res) => {
     res.send('Hello World!')
 }
 
+const createTask = (req, res) => {
+    res.send('Hello World!')
+}
+
+const getTask = (req, res) => {
+    res.send('Hello World!')
+}
+
+const updateTask = (req, res) => {
+    res.send('Hello World!')
+}
+
+const deleteTask = (req, res) => {
+    res.send('Hello World!')
+}
+
+
 module.exports={
-    getAllTasks
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask
 }
