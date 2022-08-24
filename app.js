8c95605ff3ddc6512a40a4bb692df0fe524a3448
ramses2099 <ramses2099@gmail.com>
2022-08-24 14:12:01 -0400

    added controller and routers

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const tasks = require('./routes/tasks')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+//routes
+app.use('/api/v1/tasks', tasks)
+
+// app.get('/api/v1/tasks')       --get all the tasks
+// app.post('/api/v1/tasks')      --create a new tasks
+// app.get('/api/v1/task/:id')    --get single task
+// app.patch('/api/v1/task/:id')  --update task
+// app.delete('/api/v1/task/:id') --delete task
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
