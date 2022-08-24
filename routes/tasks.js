dbecd510d2eb74f8d9c84950597dbd560a58546c
ramses2099 <ramses2099@gmail.com>
2022-08-24 19:50:07 -0400

    connection with mongo db

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,12 +1,20 @@
-const express = require('express')
-const router = express.Router()
-const { getAllTasks,
-    createTask,
-    getTask,
-    updateTask,
-    deleteTask }= require('../controllers/tasks')
+const express = require("express");
+const router = express.Router();
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+} = require("../controllers/tasks");
 
-router.route('/').get(getAllTasks).post(createTask)
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
+// app.get('/api/v1/tasks')       --get all the tasks
+// app.post('/api/v1/tasks')      --create a new tasks
+// app.get('/api/v1/task/:id')    --get single task
+// app.patch('/api/v1/task/:id')  --update task
+// app.delete('/api/v1/task/:id') --delete task
 
-module.exports = router
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
+
+module.exports = router;
